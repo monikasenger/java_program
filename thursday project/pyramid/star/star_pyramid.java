66098c42c4d35b599629a74ad33c325ca1d8af95
monikasenger <107236089+monikasenger@users.noreply.github.com>
2022-07-14 13:49:50 +0530

    Add files via upload

--- thursday project/pyramid/star/star_pyramid.java
+++ thursday project/pyramid/star/star_pyramid.java
@@ -24,18 +24,5 @@ public class star_pyramid {
 		}
 		System.out.println("");
 		}
-		space = 1;
-		for (j = 1; j<= n - 1; j++)
-		{
-		for (i = 1; i<= space; i++)
-		{
-		System.out.print(" ");
-		}
-		space++;
-		for (i = 1; i<= 2 * (n - j) - 1; i++)
-		{
-		System.out.print("*");
-		}
-		System.out.println("");
-		}}
+	}
 }
