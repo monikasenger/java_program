6c796e094a846c1acc3743332fb48dfc3e7cc7df
Monika Senger <107236089+monikasenger@users.noreply.github.com>
2022-07-29 10:44:00 +0530

    Program to input age from user and throw user-defined exception if entered age is negative In this program, we have created an exception class AgeException which extends the built-in Exception class. In main method, we read the input from user using Scanner after that we check the age in try block. If it is less than 18 then it will throw an exception other it will display message "Valid age".

--- thursday project/project(28-07-22)/age check/age_valid.java
+++ thursday project/project(28-07-22)/age check/age_valid.java
@@ -1,3 +1,8 @@
+/*Program to input age from user and throw user-defined exception if entered age is negative
+In this program, we have created an exception class AgeException which extends the built-in Exception class. 
+In main method, we read the input from user using Scanner after that we check the age in try block.
+If it is less than 18 then it will throw an exception other it will display message "Valid age".*/
+
 package exception_project;
 
 import java.util.Scanner;
@@ -13,7 +18,7 @@ public static void main(String[] args) {
 //creating age class
 	public void age()
 {
-	
+	// try catch for check the age validation
 	  try {
 		  System.out.print("Enter your age : ");
 		   age = s.nextInt();
@@ -27,7 +32,7 @@ public static void main(String[] args) {
 	//class created to check the age
 	public void check_age(int b) 
 	 {
-		 if(b < 18)
+		 if(b < 18)   
 		 {
 			 throw new ArithmeticException("\n Invalid age ...."
 			 		+ "\n age should be greater than 18"); 
