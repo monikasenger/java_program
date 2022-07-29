a8917236d5ed496eaafdbedb6cd6f3b02e9ff001
Monika Senger <107236089+monikasenger@users.noreply.github.com>
2022-07-29 10:49:49 +0530

    Write a program to accept User ID and  password checks if the password is valid or not based on password policies mention below:(Using exception Handling)  Password should not contain any space. Password should contain at least one digit(0-9). Password length should be between 8 to 15 characters. Password should contain at least one lowercase letter(a-z). Password should contain at least one uppercase letter(A-Z). Password should contain at least one special character ( @, #, %, &, !, $, etc….).

--- thursday project/project(28-07-22)/user password check/user_pass_main.java
+++ thursday project/project(28-07-22)/user password check/user_pass_main.java
@@ -1,11 +1,12 @@
+// main class for user password checking 
 package exception_project;
 
 public class user_pass_main extends user_pass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		user_pass_main obj=new user_pass_main();
-		obj.accept();
+		user_pass_main obj=new user_pass_main();  //creating the object of main class
+		obj.accept();  // call the accept class using object
 	}
 
 }
