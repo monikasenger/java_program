85e6c339689796ba8eaee7f781659b4cdf0d1470
Monika Senger <107236089+monikasenger@users.noreply.github.com>
2022-07-29 10:52:20 +0530

    Write a program to accept basic details name , Phone No , Address , user Id and password from the user . throw an exception if the user Id lenght is greater than 8 and password is not matched and should only contain the string value

--- thursday project/project(28-07-22)/display the details of user/main_class.java
+++ thursday project/project(28-07-22)/display the details of user/main_class.java
@@ -1,10 +1,12 @@
+// main class of displaying the details of user
+
 package exception_project;
 
 public class main_class extends details{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		main_class obj=new main_class();
-	obj.accept();
+		main_class obj=new main_class(); // creating object of main class
+	obj.accept(); // call the accept class using object
 	}
 }
